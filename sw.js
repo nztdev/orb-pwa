a8ab6565a396ad84f281bbb0e2b0d4ea829ff5f3
nztdev <lifeofanomadpoet@gmail.com>
2026-08-01 17:10:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'orb-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/main.css',
+  './js/app.js',
+  './js/visualEngine.js',
+  './js/audioEngine.js',
+  './manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
